5879ff4dcf83cc745bf9e5eac5225e61c90692cd
Priyanshu <priyanshujha14@gmail.com>
2023-09-20 14:24:26 +0530

    solved kadanes algorithm and next permutation problem

---
+++ Arrays/KadanesAlgorithm.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class KadanesAlgorithm {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[] nums = new int[sc.nextInt()];
+        for (int i = 0; i < nums.length; i++) {
+            nums[i] = sc.nextInt();
+        }
+        int result = maxSubArray(nums);
+        System.out.println(result);
+    }
+
+    public static int maxSubArray(int[] nums) {
+        int sum = 0;
+        int maxSum = Integer.MIN_VALUE;
+        for (int i = 0; i < nums.length; i++) {
+            sum += nums[i];
+            maxSum = Math.max(maxSum, sum);
+            if (sum < 0) {
+                sum = 0;
+            }
+        }
+        return maxSum;
+    }
+}
---
+++ Arrays/NextPermutation.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+public class NextPermutation {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[] nums = new int[sc.nextInt()];
+        for (int i = 0; i < nums.length; i++) {
+            nums[i] = sc.nextInt();
+        }
+    }
+
+    public static int[] nextPermutation(int[] nums) {
+        int[] result = new int[nums.length];
+        int i = nums.length - 2;
+        while (i >= 0 && nums[i] >= nums[i + 1]) {
+            i--;
+        }
+        if (i >= 0) {
+            int j = nums.length - 1;
+            while (j >= 0 && nums[j] <= nums[i]) {
+                j--;
+            }
+            swap(nums, i, j);
+        }
+        reverse(nums, i + 1, nums.length - 1);
+        return result;
+    }
+
+    public static void swap(int[] nums, int i, int j) {
+        int temp = nums[j];
+        nums[j] = nums[i];
+        nums[i] = temp;
+    }
+
+    public static void reverse(int[] nums, int i, int j) {
+        while (i < j) {
+            swap(nums, i++, j--);
+        }
+    }
+}
